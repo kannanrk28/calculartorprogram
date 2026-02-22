b775bed41c3f15a77bb35d6f9a6bcb9f4ce7756c
kannanrk28 <99069441+kannanrk28@users.noreply.github.com>
2026-02-22 20:33:43 +0530

    Create studentname_stage1.py

---
+++ studentname_stage1.py
@@ -0,0 +1,23 @@
+# Stage 1: Basic Calculator
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+operator = input("Enter operator (+, -, *, /): ")
+
+if operator == "+":
+    result = num1 + num2
+elif operator == "-":
+    result = num1 - num2
+elif operator == "*":
+    result = num1 * num2
+elif operator == "/":
+    if num2 == 0:
+        print("Error: Division by zero is not allowed.")
+        exit()
+    else:
+        result = num1 / num2
+else:
+    print("Invalid operator")
+    exit()
+
+print("Result =", result)
